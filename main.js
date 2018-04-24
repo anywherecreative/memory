2479eee39410e37e650294a7e97b051672ec1e13
Jeff Manning <jeff@anywherecreative.ca>
2018-04-24 19:32:18 -0400

    add cards array, click for cards and shuffle function

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+var cards = new Array();
+cards[0] = "images/ace-spades.png";
+cards[1] = "images/ace-spades.png";
+cards[2] = "images/two-spades.png";
+cards[3] = "images/two-spades.png";
+cards[4] = "images/three-spades.png";
+cards[5] = "images/three-spades.png";
+cards[6] = "images/four-spades.png";
+cards[7] = "images/four-spades.png";
+cards[8] = "images/five-spades.png";
+cards[9] = "images/five-spades.png";
+cards[10] = "images/six-spades.png";
+cards[11] = "images/six-spades.png";
+
+cards = shuffle(cards);
+
 $(document).ready(function() {
-  //$("IMG")
+  $('IMG').click(function() {
+    $(this).attr('src','images/ace-spades.png');
+  });
 });
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
